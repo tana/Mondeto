9602b3fda8c4a32978bdc96082345146ac51229a
Satoshi TANAKA <tanaka@kaji-lab.jp>
2020-06-02 03:09:01 +0900

    Create GUI for mode selection (hybrid server mode or client mode)

---
+++ Assets/Scripts/StartupController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartupController : MonoBehaviour
+{
+    public Toggle ServerToggle;
+    public Toggle ClientToggle;
+
+    public void OnStartClicked()
+    {
+        if (ServerToggle.isOn)
+        {
+            SceneManager.LoadScene("WalkServerHybrid");
+        }
+        else if (ClientToggle.isOn)
+        {
+            SceneManager.LoadScene("WalkClient");
+        }
+    }
+}
