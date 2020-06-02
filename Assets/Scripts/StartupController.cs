b5f8a762c80a7c55f44fd6a709de659ba1d655dc
Satoshi TANAKA <tanaka@kaji-lab.jp>
2020-06-02 18:06:58 +0900

    Detect batch mode and automatically start server

--- Assets/Scripts/StartupController.cs
+++ Assets/Scripts/StartupController.cs
@@ -9,6 +9,15 @@ public class StartupController : MonoBehaviour
     public Toggle ServerToggle;
     public Toggle ClientToggle;
 
+    public void Start()
+    {
+        if (Application.isBatchMode)
+        {
+            Logger.Log("StartupController", "Running as batch mode. Starting dedicated server scene");
+            SceneManager.LoadScene("WalkServer");
+        }
+    }
+
     public void OnStartClicked()
     {
         if (ServerToggle.isOn)
