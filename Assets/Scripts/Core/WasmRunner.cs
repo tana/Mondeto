4b201a9d7549a09b3e969f0318ae66d61412bb92
Satoshi TANAKA <tanaka@kaji-lab.jp>
2020-09-10 20:55:01 +0900

    Fix some mistakes (WASM-related)

--- Assets/Scripts/Core/WasmRunner.cs
+++ Assets/Scripts/Core/WasmRunner.cs
@@ -20,6 +20,14 @@ class WasmRunner : IDisposable
         }
 
         // Compile WASM
+        // Although dynamic can be used (as explained in the following URLs), we use object type instead (because we use reflections to call functions more dynamically).
+        //  https://github.com/RyanLamansky/dotnet-webassembly/blob/8c60c4a657d9caf616f1926acf10adbf26a0980b/README.md#sample-create-and-execute-a-webassembly-file-in-memory
+        //  https://github.com/RyanLamansky/dotnet-webassembly/blob/2683120c0df708404b44b89281e5843decb7347b/WebAssembly.Tests/CompilerTests.cs#L61
+        // Note (for understanding API of dotnet-webassembly):
+        //  In comments, there are some references to dotnet-webassembly test codes (WebAssembly.Tests).
+        //  These test codes use ToInstance that is not present in library (used only in their tests).
+        //      See: https://github.com/RyanLamansky/dotnet-webassembly/blob/6a19dd5816865ef06c5beb056384d11ef216ace9/WebAssembly.Tests/ModuleExtensions.cs#L85
+
         var instanceCreator = module.Compile<object>();
         instance = instanceCreator(new ImportDictionary());
 
@@ -36,7 +44,7 @@ class WasmRunner : IDisposable
         // (for example, we use "_init" instead of WASI "_initialize")
 
         // Call WASM functions using reflection.
-        var type = instance.GetType();
+        var type = instance.Exports.GetType();
         // Constructors
         var callCtorsMethod = type.GetMethod("__wasm_call_ctors", new Type[0]);
         if (callCtorsMethod != null)
@@ -45,9 +53,10 @@ class WasmRunner : IDisposable
         }
         // Initialization
         var initMethod = type.GetMethod("_init", new Type[0]);
-        if (initMethod != null)
+        if (initMethod == null)
         {
             Logger.Error("WasmRunner", "Cannot find function _init");
+            return;
         }
         initMethod.Invoke(instance, new object[0]);
     }
--- wasm/test.wat
+++ wasm/test.wat
@@ -3,7 +3,7 @@
 ;; Converted to binary format with the following command line:
 ;;  wat2wasm test.wat -o test.wasm
 (module
-    (func $_init (param) (return)
+    (func $_init (param) (result)
         ;; DO NOTHING
     )
     (export "_init" (func $_init))
