017bba85b7ae9e4d3628d394a4f8f305e531d6af
Satoshi TANAKA <tanaka@kaji-lab.jp>
2020-09-21 22:33:26 +0900

    Move log redirection from startup to each scene

---
+++ Assets/Scripts/UnityInterface/UnityLogOutput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityLogOutput : MonoBehaviour
+{
+    public void Start()
+    {
+        // If batch mode, log is only written into stdout (Console.WriteLine).
+        // If graphics is available, log is also written into Unity debug console.
+        if (!Application.isBatchMode)
+        {
+            Logger.OnLog += (type, component, msg) => {
+                Debug.Log($"[{Logger.LogTypeToString(type)}] {component}: {msg}");
+            };
+        }
+    }
+}
