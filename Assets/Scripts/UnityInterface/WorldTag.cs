92ea129bd6ce9fad5417290cf74621f52a555465
Satoshi TANAKA <tanaka@kaji-lab.jp>
2020-12-24 07:11:45 +0900

    Add skybox support

--- Assets/Scripts/UnityInterface/WorldTag.cs
+++ Assets/Scripts/UnityInterface/WorldTag.cs
@@ -4,11 +4,51 @@ public class WorldTag : MonoBehaviour, ITag
 {
     SyncObject obj;
 
+    Material skyboxMaterial;
+
     public void Setup(SyncObject syncObject)
     {
         obj = syncObject;
 
-        obj.WriteLog("WorldTag", "world tag setup completed");
+        skyboxMaterial = new Material(Shader.Find("Skybox/Panoramic"));
+
+        obj.RegisterFieldUpdateHandler("skyboxTexture", HandleSkyboxTextureUpdate);
+        obj.RegisterFieldUpdateHandler("skyboxType", HandleUpdate);
+
+        HandleUpdate();
+        HandleSkyboxTextureUpdate();
+    }
+
+    async void HandleSkyboxTextureUpdate()
+    {
+        if (obj.TryGetField("skyboxTexture", out BlobHandle textureHandle))
+        {
+            Blob blob = await obj.Node.ReadBlob(textureHandle);
+            // Texture size is automatically updated by LoadImage,
+            // so we can specify arbitrary value when creating a Texture2D.
+            //  See: https://docs.unity3d.com/ja/2019.4/ScriptReference/ImageConversion.LoadImage.html
+            var texture = new Texture2D(1, 1);
+            texture.LoadImage(blob.Data);
+            // Use WrapMode.Clamp to suppress lines of texture edges
+            // See:
+            //  https://docs.unity3d.com/ja/2018.4/Manual/HOWTO-UseSkybox.html
+            //  https://sat-box.hatenablog.jp/entry/2017/09/11/143801
+            // (FIXME: Not suppressed yet?)
+            texture.wrapMode = TextureWrapMode.Clamp;
+
+            skyboxMaterial.mainTexture = texture;
+            obj.WriteLog("WorldTag", "Skybox texture loaded");
+
+            // Use this skybox texture for both background and ambient light
+            // See: https://docs.unity3d.com/ja/2019.4/Manual/skyboxes-using.html
+            RenderSettings.skybox = skyboxMaterial;
+            RenderSettings.ambientMode = UnityEngine.Rendering.AmbientMode.Skybox;
+        }
+    }
+
+    void HandleUpdate()
+    {
+        // TODO: support skybox types other than panoramic
     }
 
     public void Cleanup(SyncObject obj)
@@ -18,5 +58,7 @@ public class WorldTag : MonoBehaviour, ITag
 
     void OnDestroy()
     {
+        obj.DeleteFieldUpdateHandler("skyboxTexture", HandleSkyboxTextureUpdate);
+        obj.DeleteFieldUpdateHandler("skyboxType", HandleUpdate);
     }
 }
