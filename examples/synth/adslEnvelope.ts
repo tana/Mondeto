272d12fc488b44a8ccf2bb2ed69a9e9ba98a3690
Satoshi TANAKA <tanaka@kaji-lab.jp>
2021-01-10 20:59:16 +0900

    ADSR envelope generator

---
+++ examples/synth/adslEnvelope.ts
@@ -0,0 +1,56 @@
+export class ADSLEnvelope {
+    private readonly fs: f32;
+    private isOn: bool = false;
+    private isReleasing: bool = false;
+    private noteTime: f32 = 0;  // time after noteOn
+    private timeAfterOff: f32 = 0;   // time after NoteOff
+    // parameters
+    attackTime: f32 = 0;    // time of linear increase between noteOn and maximum amplitude
+    decayTime: f32 = 0;     // time of linear decay between maximam amplitude and sustain level
+    sustainLevel: f32 = 1;  // amplitude during sustain state
+    releaseTime: f32 = 0;   // time of linear decay between noteOff and complete stop
+
+    constructor(fs: f32) {
+        this.fs = fs;
+    }
+
+    compute(): f32 {
+        let value: f32;
+        if (this.isOn) {
+            if (this.noteTime < this.attackTime) {    // Attack state
+                value = this.noteTime / this.attackTime;
+            } else if (this.noteTime < this.decayTime) {  // Decay state
+                value = 1 - (1 - this.sustainLevel) * (this.noteTime - this.attackTime) / this.decayTime;
+            } else {    // Sustain state
+                value = this.sustainLevel;
+            }
+
+            this.noteTime += 1 / this.fs;
+        } else if (this.isReleasing) {   // Release state
+            if (this.timeAfterOff >= this.releaseTime) {
+                this.isReleasing = false;    // sound is completely stopped
+            }
+
+            value = this.sustainLevel * (1 - this.timeAfterOff / this.releaseTime);
+
+            this.timeAfterOff += 1 / this.fs;
+        } else {
+            value = 0;
+        }
+
+        return value;
+    }
+
+    noteOn(): void {
+        this.noteTime = 0;
+        this.isOn = true;
+    }
+
+    noteOff(): void {
+        if (this.isOn) {
+            this.isOn = false;
+            this.isReleasing = true;
+            this.timeAfterOff = 0;
+        }
+    }
+}
--- examples/synth/subtractiveSynth.ts
+++ examples/synth/subtractiveSynth.ts
@@ -1,31 +1,37 @@
 import { SawtoothOscillator } from "./sawtoothOscillator";
 import { BiquadFilter } from "./biquadFilter";
+import { ADSLEnvelope } from "./adslEnvelope";
 
 // Monophonic subtractive synthesizer
 export class SubtractiveSynth {
     private readonly fs: f32;   // sampling freq
     private readonly osc: SawtoothOscillator;
     private readonly filter: BiquadFilter;
-    private amp: f32 = 0.0;
+    private readonly envelope: ADSLEnvelope;
 
     constructor(fs: f32) {
         this.fs = fs;
         this.osc = new SawtoothOscillator(this.fs);
         this.filter = new BiquadFilter(this.fs);
+        this.envelope = new ADSLEnvelope(this.fs);
 
         this.filter.setCharacteristics(1000, 0.5);
+        this.envelope.attackTime = 0.1;
+        this.envelope.decayTime = 0.5;
+        this.envelope.sustainLevel = 0.3;
+        this.envelope.releaseTime = 0.0;
     }
 
     compute(): f32 {
-        return this.filter.compute(this.osc.compute());
+        return this.filter.compute(this.osc.compute()) * this.envelope.compute();
     }
 
     noteOn(freq: f32): void {
         this.osc.freq = freq;
-        this.amp = 1.0;
+        this.envelope.noteOn();
     }
 
     noteOff(): void {
-        this.amp = 0.0;
+        this.envelope.noteOff();
     }
 }
