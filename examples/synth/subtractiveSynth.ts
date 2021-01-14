54348c730ccfad5cf58e9392ba93fdc4b1d52a38
Satoshi TANAKA <tanaka@kaji-lab.jp>
2021-01-14 21:58:48 +0900

    Parameter setting methods for synth. Also a key is added in synth scene.

--- examples/synth/subtractiveSynth.ts
+++ examples/synth/subtractiveSynth.ts
@@ -8,6 +8,8 @@ export class SubtractiveSynth {
     private readonly osc: SawtoothOscillator;
     private readonly filter: BiquadFilter;
     private readonly envelope: ADSLEnvelope;
+    private filterRelativeFreq: f32;
+    private filterQ: f32;
 
     constructor(fs: f32) {
         this.fs = fs;
@@ -15,11 +17,9 @@ export class SubtractiveSynth {
         this.filter = new BiquadFilter(this.fs);
         this.envelope = new ADSLEnvelope(this.fs);
 
-        this.filter.setCharacteristics(1000, 0.5);
-        this.envelope.attackTime = 0.1;
-        this.envelope.decayTime = 0.5;
-        this.envelope.sustainLevel = 0.3;
-        this.envelope.releaseTime = 0.0;
+        this.setAmpADSR(0.1, 0.5, 0.3, 0.2);
+        this.setFilterRelativeFreq(2);
+        this.setFilterQ(0.5);
     }
 
     compute(): f32 {
@@ -28,10 +28,33 @@ export class SubtractiveSynth {
 
     noteOn(freq: f32): void {
         this.osc.freq = freq;
+        this.setFilter();
         this.envelope.noteOn();
     }
 
     noteOff(): void {
         this.envelope.noteOff();
+    }
+
+    setAmpADSR(attackTime: f32, decayTime: f32, sustainLevel: f32, releaseTime: f32): void {
+        this.envelope.attackTime = attackTime;
+        this.envelope.decayTime = decayTime;
+        this.envelope.sustainLevel = sustainLevel;
+        this.envelope.releaseTime = releaseTime;
+    }
+
+    setFilterRelativeFreq(relativeFreq: f32): void {
+        this.filterRelativeFreq = relativeFreq;
+        this.setFilter();
+    }
+
+    setFilterQ(q: f32): void {
+        this.filterQ = q;
+        this.setFilter();
+    }
+
+    private setFilter(): void {
+        const filterFreq = this.osc.freq * this.filterRelativeFreq;
+        this.filter.setCharacteristics(filterFreq, this.filterQ);
     }
 }
